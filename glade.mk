dfa954ea8ff2caf04a78d6b90086fd5ada397e30
rohan <purohit.rohan@gmail.com>
2015-04-19 04:59:44 +0200

    glade-ify

---
+++ glade.mk
@@ -0,0 +1,7 @@
+$(call inherit-product, device/motorola/titan/full_titan.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/glade/configs/common.mk)
+
+PRODUCT_RELEASE_NAME := titan
+PRODUCT_NAME := glade_titan
